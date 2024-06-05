21f91103736330c29f2064b6f76205db2d194104
VillalbaLeandro <exemprary@gmail.com>
2024-06-05 11:18:44 -0300

    REspaldo del apirestfull en nest de pokedex

--- src/common/pipes/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -10,7 +10,7 @@ import { isValidObjectId } from 'mongoose';
 export class ParseMongoIdPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
     if (!isValidObjectId(value)) {
-      throw new BadRequestException('El id ingresado no es un mongo id valido');
+      throw new BadRequestException('El id ingresado no es un mongo id valido, lo siento');
     }
     return value.toUpperCase();
   }
